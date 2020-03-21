44ddbf87a6421be6fb67cb77c92db169fc31c773
Stanimir Vasilev <stanimirvasilev@yahoo.com>
2020-03-21 12:46:42 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -18,7 +18,7 @@ class CashRegister
   
   def apply_discount
    #we need to treath it as a flow 
-    @total = @total - (@total.to_f).percent_of(total)
+    @total = @total - (discount.to_f).percent_of(@total.to_f)
   # total = totala - (X)% discount ot totala 
   end
   
