a2db183308bcd89fae8d60e6bef04348da585b54
Stanimir Vasilev <stanimirvasilev@yahoo.com>
2020-03-21 12:27:35 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,25 @@
 
+class CashRegister
+  
+  attr_accessor :total, :item, :discount
+  
+  def initialize(discount = 0)
+    @item = []
+    @total = 0
+    @discount = discount
+  end
+  
+  def add_item(item, cost = 0, number_items = 1)
+    @total += cost * number_items
+    @item << item 
+   
+  end
+  
+  
+  def apply_discount
+    @total = @total - 
+   
+  end
+  
+  
+end
